f0e7556627c573c04b4f546d58b2a20b67fcb2fe
Erik Schalburg <erik@amicus.io>
2021-12-07 08:11:10 -0500

    Day 6

--- src/java/org/cosmiclovers/advent2021/Main.java
+++ src/java/org/cosmiclovers/advent2021/Main.java
@@ -2,20 +2,18 @@ package org.cosmiclovers.advent2021;
 
 import java.util.Calendar;
 
-import static java.util.Calendar.getInstance;
-
 public class Main {
     public static void main(String[] args) {
         int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
 
-        String className = "org.cosmiclovers.advent2021.Day" + day;
+        String className = args[0];
 
         try {
-            Assignment a = (Assignment) Class.forName(className).newInstance();
-            System.out.format("%s instantiated, executing.\n", className);
+            Assignment a = (Assignment) Class.forName(className).getDeclaredConstructor().newInstance();
+            System.out.format("%s instantiated, executing.%n", className);
             a.go(args);
         } catch (Exception e) {
-            System.out.format("Could not load class %s, exiting.\n", className);
+            System.out.format("Could not load class %s, exiting.%n", className);
             e.printStackTrace();
         }
 
